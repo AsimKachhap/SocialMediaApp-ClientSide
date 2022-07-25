b1842570832dea69ba1675d71ca0db4f87adfd2e
AsimKachhap <asim26k@gmail.com>
2022-07-25 12:12:49 +0530

    middlewares created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, {useEffect} from 'react';
+import {getPosts} from './actions/posts';
+import Posts from './Posts/Posts';
+import Form from './Form/Form';
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+import memories from './images/memories.png';
+import useStyles from './styles';
 
 function App() {
+
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  
+  useEffect(()=>{
+      dispatch(getPosts());
+  },[dispatch]);
+
   return (
-    <h1>App</h1>
+    <Container maxWidth="lg">
+      <AppBar className={classes.appbar} position="static" color="inherit">
+       <Typography className={classes.heading} varient="h2" align="center">Memories</Typography>
+        <img  className={classes.image} src={memories} alt="memories" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+            
+            <Grid item xs={12} sm={7}>
+              <Posts/>
+            </Grid>
+
+            <Grid item xs={12} sm={4}>
+              <Form/>
+            </Grid>
+
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   )
 }
 
-export default App
+export default App;
