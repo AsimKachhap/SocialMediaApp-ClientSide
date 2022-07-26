c3da534453b938702b983fa25ac8744a0013d156
AsimKachhap <asim26k@gmail.com>
2022-07-26 12:02:05 +0530

    fixed errors on path /posts

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import { TextField, Button, Typography,Paper } from '@material-ui/core';
-import { FileBase } from 'react-file-base64';
+import FileBase from 'react-file-base64';
 import useStyles from './styles';
 
 const Form = () => {
@@ -17,14 +17,14 @@ const Form = () => {
 
   return (
     <Paper className={classes.paper}>
-      <form autoComplete='off' noValidate className={classes.form} onSubmit={handleSubmit}>
+      <form autoComplete='off' noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
         <TextField name="creator"  variant="outlined"  fullWidth label="Creator"  value={postData.creator}  onChange={((e)=> setPostData({...postData, creator: e.target.value}))}/>
         <TextField name="title"  variant="outlined"  fullWidth label="Title" value={postData.title}  onChange={((e)=> setPostData({...postData, title: e.target.value}))}/>
         <TextField name="message"  variant="outlined"  fullWidth label="Message"  value={postData.message}  onChange={((e)=> setPostData({...postData, message: e.target.value}))}/>
         <TextField name="tags"  variant="outlined"  fullWidth label="Tags" value={postData.title}  onChange={((e)=> setPostData({...postData, tags: e.target.value}))}/>
         <div className={classes.fileInput}> <FileBase type="file" multiple={false} onDone={({base64})=>setPostData({...postData, selectedFile:base64 })} /> </div>
-        <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit">Submit</Button>
-        <Button  variant="contained" color="secondary" size="small" onClick={onClick}>Clear</Button>
+        <Button className={classes.buttonSubmit} variant="contained"  fullWidth color="primary" size="large" type="submit">Submit</Button>
+        <Button  variant="contained" fullWidth color="secondary" size="small" onClick={onClick}>Clear</Button>
       </form>
     </Paper>
   );
